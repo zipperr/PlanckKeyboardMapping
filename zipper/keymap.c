49423a0e79a58d01781bb89ff1a2d6754297eb5c
zipperr <zipperr@i.softbank.jp>
2018-09-09 16:38:24 +0900

    リファクタ

--- zipper/keymap.c
+++ zipper/keymap.c
@@ -7,9 +7,9 @@ extern keymap_config_t keymap_config;
 
 enum planck_layers {
   _US_QWERTY,
-  _JIS_QWERTY,
   _US_LOWER,
   _US_RAISE,
+  _JIS_QWERTY,
   _JIS_LOWER,
   _JIS_RAISE,
   _ADJUST,
@@ -30,10 +30,10 @@ enum user_macro {
   JIS_KHKR
 };
 
-#define M_US_EMHL MACROTAP(US_EMHL)
-#define M_US_KHKR MACROTAP(US_KHKR)
-#define M_JIS_EMHL MACROTAP(JIS_EMHL)
-#define M_JIS_KHKR MACROTAP(JIS_KHKR)
+#define US_LOWER MACROTAP(US_EMHL)
+#define US_RAISE MACROTAP(US_KHKR)
+#define JIS_LOWER MACROTAP(JIS_EMHL)
+#define JIS_RAISE MACROTAP(JIS_KHKR)
 #define MOUSE MO(_MOUSE)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -49,10 +49,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_US_QWERTY] = LAYOUT_planck_grid(
-    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,      KC_T,   KC_Y,   KC_U,      KC_I,    KC_O,    KC_P,    KC_BSPC,
-    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,      KC_G,   KC_H,   KC_J,      KC_K,    KC_L,    KC_SCLN, KC_ENT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,      KC_B,   KC_N,   KC_M,      KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
-    MOUSE,   KC_LCTL, KC_LALT, KC_LGUI, M_US_EMHL, KC_SPC, KC_SPC, M_US_KHKR, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,   KC_Y,   KC_U,     KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,     KC_G,   KC_H,   KC_J,     KC_K,    KC_L,    KC_SCLN, KC_ENT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,   KC_N,   KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
+    MOUSE,   KC_LCTL, KC_LALT, KC_LGUI, US_LOWER, KC_SPC, KC_SPC, US_RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Lower(US配列)
@@ -103,10 +103,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_JIS_QWERTY] = LAYOUT_planck_grid(
-    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,       KC_T,   KC_Y,   KC_U,       KC_I,    KC_O,    KC_P,     KC_BSPC,
-    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,       KC_G,   KC_H,   KC_J,       KC_K,    KC_L,    JIS_SCLN, KC_ENT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,       KC_B,   KC_N,   KC_M,       KC_COMM, KC_DOT,  KC_SLSH,  JIS_QUOT,
-    MOUSE,   KC_LCTL, KC_LALT, KC_LGUI, M_JIS_EMHL, KC_SPC, KC_SPC, M_JIS_KHKR, KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT
+    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,      KC_T,   KC_Y,   KC_U,      KC_I,    KC_O,    KC_P,     KC_BSPC,
+    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,      KC_G,   KC_H,   KC_J,      KC_K,    KC_L,    JIS_SCLN, KC_ENT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,      KC_B,   KC_N,   KC_M,      KC_COMM, KC_DOT,  KC_SLSH,  JIS_QUOT,
+    MOUSE,   KC_LCTL, KC_LALT, KC_LGUI, JIS_LOWER, KC_SPC, KC_SPC, JIS_RAISE, KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT
 ),
 
 /* Lower(JIS配列)
