5f88e06bc82735a4ff6136c72ecacd64c0c56554
zipperr <zipperr@i.softbank.jp>
2019-02-17 22:44:19 +0900

    a

--- zipper/keymap.c
+++ zipper/keymap.c
@@ -8,7 +8,6 @@ enum planck_layers {
   _US_LOWER,
   _US_RAISE,
   _JIS_QWERTY,
-  _JIS_QWERTY_2,
   _JIS_LOWER,
   _JIS_RAISE,
   _ADJUST
@@ -19,7 +18,6 @@ enum planck_keycodes {
   US_LOWER,
   US_RAISE,
   JIS_QWERTY,
-  JIS_QWERTY2,
   JIS_LOWER,
   JIS_RAISE,
   JIS_QUOT,
@@ -55,12 +53,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     CTL_TAB, KC_A,     KC_S,    KC_D,    KC_F,      KC_G,   KC_H,   KC_J,      KC_K,    KC_L,    JIS_SCLN, KC_ENT,
     KC_LSFT, KC_Z,     KC_X,    KC_C,    KC_V,      KC_B,   KC_N,   KC_M,      KC_COMM, KC_DOT,  KC_SLSH,  JIS_QUOT,
     ADJUST,  KC_LCTRL, KC_LALT, KC_LGUI, JIS_LOWER, KC_SPC, KC_SPC, JIS_RAISE, KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT
-),
-[_JIS_QWERTY_2] = LAYOUT_planck_grid(
-    KC_ESC,  KC_Q,     KC_W,    KC_E,    KC_R,      KC_T,   KC_Y,   KC_U,      KC_I,    KC_O,    KC_P,     KC_BSPC,
-    CTL_TAB, KC_A,     KC_S,    KC_D,    KC_F,      KC_G,   KC_H,   KC_J,      KC_K,    KC_L,    JIS_SCLN, KC_ENT,
-    KC_LSFT, KC_Z,     KC_X,    KC_C,    KC_V,      KC_B,   KC_N,   KC_M,      KC_COMM, KC_DOT,  KC_UP,    JIS_QUOT,
-    ADJUST,  KC_LCTRL, KC_LALT, KC_LGUI, JIS_LOWER, KC_SPC, KC_SPC, JIS_RAISE, KC_SLSH, KC_LEFT, KC_DOWN,  KC_RGHT
 ),
 
 /* Lower
@@ -123,15 +115,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_planck_grid(
-    KC_F1,      KC_F2,       KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-    US_QWERTY,  NKRO_TOG,    _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______,
-    JIS_QWERTY, JIS_QWERTY2, _______, _______, _______, _______, KC_WH_D, KC_BTN1, KC_BTN2, KC_WH_U, _______, _______,
-    RESET,      AU_TOG,      CK_TOGG, MU_TOG,  _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
+    KC_F1,      KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+    US_QWERTY,  NKRO_TOG, _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______,
+    JIS_QWERTY, _______,  _______, _______, _______, _______, KC_WH_D, KC_BTN1, KC_BTN2, KC_WH_U, _______, _______,
+    RESET,      AU_TOG,   CK_TOGG, MU_TOG,  _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
 )};
 
 #ifdef AUDIO_ENABLE
-    float tone_us_layer_on[][2] = SONG(ONE_UP_SOUND);
-    float tone_jis_layer_on[][2] = SONG(ONE_UP_SOUND);
+    float tone_layer_on[][2] = SONG(ONE_UP_SOUND);
 #endif
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -146,7 +137,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case US_QWERTY:
             if (record->event.pressed) {
                 #ifdef AUDIO_ENABLE
-                    PLAY_SONG(tone_us_layer_on);
+                    PLAY_SONG(tone_layer_on);
                 #endif
                 set_single_persistent_default_layer(_US_QWERTY);
             }
@@ -155,18 +146,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case JIS_QWERTY:
             if (record->event.pressed) {
                 #ifdef AUDIO_ENABLE
-                    PLAY_SONG(tone_jis_layer_on);
+                    PLAY_SONG(tone_layer_on);
                 #endif
                 set_single_persistent_default_layer(_JIS_QWERTY);
-            }
-            return false;
-            break;
-        case JIS_QWERTY2:
-            if (record->event.pressed) {
-                #ifdef AUDIO_ENABLE
-                    PLAY_SONG(tone_jis_layer_on);
-                #endif
-                set_single_persistent_default_layer(_JIS_QWERTY_2);
             }
             return false;
             break;
@@ -241,9 +223,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case JIS_QUOT:
             if (record->event.pressed) {
                 if (is_shift) {
-                    register_code(KC_LSFT);
+                register_code(KC_LSFT);
                     register_code(KC_2);
-
                 } else {
                     register_code(KC_LSFT);
                     register_code(KC_7);
@@ -251,6 +232,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             } else {
                 unregister_code(KC_2);
                 unregister_code(KC_7);
+                unregister_code(KC_LSFT);
             }
             return false;
             break;
